173d95f0a62708091ea9ccb8c0207dda581840d2
Sergio <sergylozanodelrio@gmail.com>
2018-11-24 19:17:37 +0100

    first commit

---
+++ process-message.js
@@ -0,0 +1,49 @@
+const Dialogflow = require('dialogflow');
+const Pusher = require('pusher');
+
+//DialogFlow project Ids
+const projectId = 'minds-c37d5'; 
+const sessionId = '123456';
+const languageCode = 'en-US';
+
+const config = {
+    credentials: {
+      private_key: process.env.DIALOGFLOW_PRIVATE_KEY,
+      client_email: process.env.DIALOGFLOW_CLIENT_EMAIL,
+    },
+};
+
+const pusher = new Pusher({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_APP_KEY,
+    secret: process.env.PUSHER_APP_SECRET,
+    cluster: process.env.PUSHER_APP_CLUSTER,
+    encrypted: true
+});
+
+const sessionClient = new Dialogflow.SessionsClient(config);
+
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+const processMessage = message => {
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: message,
+                languageCode
+            },
+        },
+    };
+
+    sessionClient.detectIntent(request).then(responses => {
+        const result = responses[0].queryResult;
+        return pusher.trigger('bot', 'bot-response', {
+            message: result.fulfillmentText,
+        });
+    }).catch(err => {
+        console.error('ERROR', err);
+    });
+}
+
+module.exports = processMessage;
